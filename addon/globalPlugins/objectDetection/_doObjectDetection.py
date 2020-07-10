a856a0d6561697d117e4a0b684c2a11112f8ec38
Shubham <shubhamdjain7@gmail.com>
2020-07-10 14:04:20 +0530

    Created super doObjectDetection class

--- addon/globalPlugins/objectDetection/_doObjectDetection.py
+++ addon/globalPlugins/objectDetection/_doObjectDetection.py
@@ -6,8 +6,7 @@ import contentRecog
 from ._YOLOv3 import YOLOv3Detection
 from ._DETR import DETRDetection
 
-class doDetectionTinyYOLOv3(contentRecog.ContentRecognizer):
-
+class doObjectDetection(contentRecog.ContentRecognizer):
 	def recognize(self, pixels, imgInfo, onResult):
 		bmp = wx.EmptyBitmap(imgInfo.recogWidth, imgInfo.recogHeight, 32)
 		bmp.CopyFromBuffer(pixels, wx.BitmapBufferFormat_RGB32)
@@ -31,64 +30,23 @@ class doDetectionTinyYOLOv3(contentRecog.ContentRecognizer):
 	def cancel(self):
 		self._onResult = None
 
+	def detect(self, imagePath):
+		raise NotImplementedError
+
+
+class doDetectionTinyYOLOv3(doObjectDetection):
 	def detect(self, imagePath):
 		result = YOLOv3Detection(imagePath, tiny=True).getSentence()
 		return contentRecog.SimpleTextResult(result)
 
-class doDetectionYOLOv3(contentRecog.ContentRecognizer):
 
-	def recognize(self, pixels, imgInfo, onResult):
-		bmp = wx.EmptyBitmap(imgInfo.recogWidth, imgInfo.recogHeight, 32)
-		bmp.CopyFromBuffer(pixels, wx.BitmapBufferFormat_RGB32)
-		self._imagePath = tempfile.mktemp(prefix="nvda_ObjectDetect_", suffix=".jpg")
-		bmp.SaveFile(self._imagePath, wx.BITMAP_TYPE_JPEG)
-		self._onResult = onResult
-		t = threading.Thread(target=self._bgRecog)
-		t.daemon = True
-		t.start()
-
-	def _bgRecog(self):
-		try:
-			result = self.detect(self._imagePath)
-		except Exception as e:
-			result = e
-		finally:
-			os.remove(self._imagePath)
-		if self._onResult:
-			self._onResult(result)
-
-	def cancel(self):
-		self._onResult = None
-
+class doDetectionYOLOv3(doObjectDetection):
 	def detect(self, imagePath):
 		result = YOLOv3Detection(imagePath, tiny=False).getSentence()
 		return contentRecog.SimpleTextResult(result)
 
-class doDetectionDETR(contentRecog.ContentRecognizer):
 
-	def recognize(self, pixels, imgInfo, onResult):
-		bmp = wx.EmptyBitmap(imgInfo.recogWidth, imgInfo.recogHeight, 32)
-		bmp.CopyFromBuffer(pixels, wx.BitmapBufferFormat_RGB32)
-		self._imagePath = tempfile.mktemp(prefix="nvda_ObjectDetect_", suffix=".jpg")
-		bmp.SaveFile(self._imagePath, wx.BITMAP_TYPE_JPEG)
-		self._onResult = onResult
-		t = threading.Thread(target=self._bgRecog)
-		t.daemon = True
-		t.start()
-
-	def _bgRecog(self):
-		try:
-			result = self.detect(self._imagePath)
-		except Exception as e:
-			result = e
-		finally:
-			os.remove(self._imagePath)
-		if self._onResult:
-			self._onResult(result)
-
-	def cancel(self):
-		self._onResult = None
-
+class doDetectionDETR(doObjectDetection):
 	def detect(self, imagePath):
 		result = DETRDetection(imagePath).getSentence()
 		return contentRecog.SimpleTextResult(result)
